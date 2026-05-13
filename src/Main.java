657a747366cb0197d8444fe959bed41e637c50f9
DobrininskyVV <vvdobryninskuy@gmail.com>
2026-05-13 20:50:35 +0300

    correction of comments

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,31 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 //Задача №2
-        dog = dog +4;
-        cat = cat +4;
-        paper = paper +4;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
 //Задача №3
-        dog = dog -3.5;
-        cat = cat -1.6;
-        paper = paper -7639;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 //Задача №4
         var friend = 19;
         System.out.println(friend);
-        friend = friend +2;
+        friend = friend + 2;
         System.out.println(friend);
-        friend = (friend +2)/7;
+        friend = friend / 7;
         System.out.println(friend);
 //Задача №5
         var frog = 3.5;
         System.out.println(frog);
-        frog = frog *10;
+        frog = frog * 10;
         System.out.println(frog);
         frog = frog / 3.5;
         System.out.println(frog);
-        frog = frog +4;
+        frog = frog + 4;
         System.out.println(frog);
 //Задача №6
         var massFirstBoxer = 78.2;
@@ -42,7 +42,7 @@ public class Main {
         System.out.println("Разница между массами двух бойцов " + differenceBoxers + " кг.");
 //Задача №7
         var remainderForDivision = massSecondBoxer % massFirstBoxer;
-        System.out.println("Остаток от деления между двумя весами " +remainderForDivision +" кг.");
+        System.out.println("Остаток от деления между двумя весами " + remainderForDivision + " кг.");
 //Задача №8 часть №1
         var totalHours = 640;
         var oneEmployeeWorks = 8;
@@ -51,6 +51,6 @@ public class Main {
 //Задача №8 часть №2
         var increaseStaff = totalEmployees + 94;
         var totalHoursWithNewStaff = increaseStaff * 8;
-        System.out.println("Если в компании работает " + increaseStaff + " человека, то всего " +totalHoursWithNewStaff +" часа работы может быть поделено между сотрудниками.");
+        System.out.println("Если в компании работает " + increaseStaff + " человека, то всего " + totalHoursWithNewStaff + " часа работы может быть поделено между сотрудниками.");
     }
 }
