6c5dc897d1d155b1a7befcb66457d88f9343ad4a
DobrininskyVV <vvdobryninskuy@gmail.com>
2026-06-10 12:17:09 +0300

    Correction of comments

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,14 @@ public class Main {
             System.out.println("На улице " + temp + " градусов, можно идти без шапки.");
         }
         // Задача №3
-        short speed = 60;
+        short speed = 59;
         if (speed > 60) {
             System.out.println("Если скорость " + speed + " то придется заплатить штраф.");
         } else {
             System.out.println("Если скорость " + speed + " можно ездить спокойно.");
         }
         // Задача №4
-        byte age1 = 24;
+        byte age1 = 25;
         if (age1 >= 2 && age1 <= 6) {
             System.out.println("Если возраст человека равен " + age1 + " то ему нужно ходить в детский сад.");
         }
@@ -32,6 +32,9 @@ public class Main {
         }
         if (age1 >= 18 && age1 <= 24) {
             System.out.println("Если возраст человека равен " + age1 + " то ему нужно ходить в университет.");
+        }
+        if (age1 > 24) {
+            System.out.println("Если возраст человека равен " + age1 + " то ему нужно ходить на работу.");
         }
 // Задача №5
         byte age2 = 15;
@@ -45,7 +48,7 @@ public class Main {
             System.out.println("Если возраст ребенка равен " + age2 + " то он может кататься без сопровождения взрослого.");
         }
 // Задача №6
-        int peopleInTheCarriage = 102;
+        int peopleInTheCarriage = 103;
         int seat = 60 - peopleInTheCarriage;
         int stand = 102 - peopleInTheCarriage;
         if (peopleInTheCarriage == 0) {
@@ -61,13 +64,15 @@ public class Main {
             System.out.println("Сейчас в вагоне  " + peopleInTheCarriage + " человека. Вагон уже полностью забит.");
         }
         // Задача №7
-        int one = 555;
-        int two = 4442;
-        int three = 33322;
-        if (one > two && one > three) System.out.println("Самое большое число: "+one +".");
-        else if (two > one && two > three) System.out.println("Самое большое число: " +two +".");
-        else {
-            System.out.println("Самое большое число: " +three +".");
+        int one = 55533222;
+        int two = 44424422;
+        int three = 33322222;
+        if (one > two && one > three) {
+            System.out.println("Самое большое число: " + one + ".");
+        } else if (two > one && two > three) {
+            System.out.println("Самое большое число: " + two + ".");
+        } else {
+            System.out.println("Самое большое число: " + three + ".");
         }
     }
 }
